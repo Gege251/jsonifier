963b4f987873465e0d2d1bfe87fd9f267872afa4
Gergo <gege251@gmail.com>
2017-09-03 19:40:10 +0900

    help function added

--- jsonifier.js
+++ jsonifier.js
@@ -8,44 +8,77 @@ const modules		= [
 	{
 		keys: [ 'init' ],
 		path: './lib/init',
-		args: [ directory ] },
+		args: [ directory ],
+		description: 'Initialize project folder.'
+	},
 	{
 		keys: [ 'new', 'n' ],
 		path: './lib/newChange',
-		args: [ directory, argv._[1] ] },
+		args: [ directory, argv._[1] ],
+		description: 'Create a new change folder.'
+	},
 	{
 		keys: [ 'add', '+'],
 		path: './lib/addFileToChange',
-		args: [ directory, argv._[1] ] },
+		args: [ directory, argv._[1] ],
+		description: 'Add a new file to change.'
+	},
 	{
 		keys: [ 'remove', '-'],
 		path: './lib/removeFromChange',
-		args: [ directory, argv._[1] ] },
+		args: [ directory, argv._[1] ],
+		description: 'Remove a file from change'
+	},
 	{
 		keys: [ 'archive', 'zip'],
 		path: './lib/archiveChange',
-		args: [ directory ] },
+		args: [ directory ],
+		description: 'Archive a change to zip file.'
+	},
 	{
 		keys: [ 'watcher', 'w' ],
 		path: './lib/watcher',
-		args: [ directory ] },
+		args: [ directory ],
+		description: 'Start watching files for changes.'
+	},
 	{
 		keys: [ 'ls' ],
 		path: './lib/list',
-		args: [ directory, argv.l, argv.f, argv.r ] },
+		args: [ directory, argv.l, argv.f, argv.r ],
+		description: 'List files in the change. Options: -l -f -r'
+	},
 	{
 		keys: [ 'createdirs', 'crd' ],
 		path: './lib/createDirs',
-		args: [ directory ] },
+		args: [ directory ],
+		description: 'Create directories for change folder.'
+	},
 	{
 		keys: [ 'stats', 's' ],
 		path: './lib/stats',
-		args: [ directory, argv.r ]	}
+		args: [ directory, argv.r ],
+		description: 'Write out statistics about current project.'
+	}
 ]
 
 function loadModule(moduleName) {
 	const module = modules.find(module => module.keys.includes(moduleName));
-	(require(module.path))(...module.args);
+	if (module) {
+		(require(module.path))(...module.args);
+	} else {
+		// Listing all command options
+		console.log('Jsonifier commands:\n');
+
+		modules.forEach(module => module.keys = module.keys.join(', '));
+		var longestOption = modules
+			.map(module => module.keys.length)
+			.reduce((m1, m2) => Math.max(m1, m2));
+
+		modules.forEach(module => {
+			let spaces = ' '.repeat(longestOption - module.keys.length + 2);
+			console.log('\t' + module.keys + spaces + module.description);
+		})
+	}
 }
 
 loadModule(argv._[0]);
